09ba0fc6e05055a46c22dda78f6e42e5f8789036
Chung-Un <chungunyum23@gmail.com>
2026-03-19 20:54:02 -0600

    trigger obligatorio

---
+++ database/triggers/TRIGGERS.sql
@@ -0,0 +1,29 @@
+CREATE OR ALTER TRIGGER tgr_crear_subcategoria_defecto
+on categorias
+after insert
+as 
+begin 
+	insert into subcategorias(
+        id_categoria,
+        nombre_subcategoria,
+        estado_subcategoria,
+        subcategoria_por_defecto,
+        creado_por,
+        modificado_por,
+        creado_en,
+        modificado_en
+    )
+
+    select
+     i.id_categoria,
+        'Default',      
+        1,              
+        1,             
+        i.creado_por,
+        i.creado_por,
+        GETDATE(),
+        GETDATE()
+    from inserted i;   
+end
+go
+        
